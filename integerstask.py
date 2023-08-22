282bcea8b5ae5f7b2aee7a2d8080b5288e229c72
Eric Omondi <ericoochieng456@gmail.com>
2023-08-22 17:05:50 +0300

    numbers

---
+++ integerstask.py
@@ -0,0 +1,26 @@
+# Convert a float to an integer with an inbuilt function
+# convert to 57
+num = 56.8926
+numint = int(num)
+print(numint)
+numround = round(num, 0)
+print(numround)
+
+# Convert the float number
+#  to 56.89
+
+num2 = 56.8926
+num2conv = round(num2, 2)
+print(num2conv)
+
+
+# Convert to 56.893
+num3 = 56.8926
+num3conv = round(num3, 3)
+print(num3conv)
+
+#  Convert to 8.926
+
+num4 = 56.8926
+num4conv = num4 ** 10
+print(num4conv) 
