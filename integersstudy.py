a37ce48f4f429a3c4b006d2de30c8e915393fe0f
Eric Omondi <ericoochieng456@gmail.com>
2023-08-22 15:42:23 +0300

    work done

---
+++ integersstudy.py
@@ -0,0 +1,58 @@
+# Float and Integers operations
+# Addition
+x = 3
+y = 2
+z = x + y
+print(z)
+
+# Subtraction
+
+x = 3
+y = 5
+z = x - y
+print(z)
+
+# Multiplication
+x =10
+y =5
+z = x * y
+
+print(z)
+
+# division (/)
+x = 20
+y = 5
+z = x / y
+print(z)
+ 
+#  Exponential operator (**)
+x = 10
+z = 2
+y = x ** z
+print(y)
+
+
+
+
+#  Modulo operator (Returns the remainder of a division)
+x = 23
+y = 5
+z = x % y
+print(z)
+
+#  floor operator(/) (Division that result into a whole no)
+x = 33
+y = 10
+z = x // y
+print(z)
+
+# Round
+# round (value, decimals)
+
+number = 345.876932032
+numr = round(number, 2)
+print(numr)
+
+
+
+
