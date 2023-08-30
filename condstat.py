69e7ae0bda1e2239f83f5591bc4b15156e7703ac
wycliffe <ericoochieng456@gmail.com>
2023-08-30 06:14:01 +0300

    after class

---
+++ condstat.py
@@ -0,0 +1,7 @@
+
+age = 30
+
+if age >= 18:
+ print("You can vote")
+else:
+ print("Not eligible")
---
+++ myoperators.py
@@ -0,0 +1,8 @@
+x = 3
+x = x + 3
+# same as 
+x+=3
+
+cars = ("bwm", "ford")
+
+print("subaru" not in cars)
