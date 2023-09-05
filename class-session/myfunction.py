0785e43ca65a7c8f8de5582e1b4d0a0cfcef11df
wycliffe <ericoochieng456@gmail.com>
2023-09-05 05:58:38 +0300

    OOP study

---
+++ class-session/myfunction.py
@@ -0,0 +1,7 @@
+def calculator(t1, t2):
+    x = t1 + t2
+    
+    return x
+
+total = calculator(5, 10)
+print(total)
--- tasks/test.py
+++ tasks/test.py
@@ -1,4 +1,17 @@
-num = 23.4655363
+import calendar
+from datetime import date
 
-rnum = round(num,2)
-print(rnum)
+def sub_mnth():
+    today = date.today()
+    month = int(today.strftime("%m"))
+
+
+
+# month = 2   # February
+# year = 2023
+ 
+# num_days = calendar.monthrange(year, month)[1]
+ 
+# print("Number of days is",num_days) 
+
+print(month)
